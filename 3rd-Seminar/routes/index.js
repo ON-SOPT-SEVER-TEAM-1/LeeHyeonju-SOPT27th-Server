7d09144265018a4435a1b7213b0dbcf5115553a4
Hyeonju Lee <winnerhj1109@naver.com>
2020-10-31 14:24:44 +0900

    [3차 세미나] level-1

--- 3rd-Seminar/routes/index.js
+++ 3rd-Seminar/routes/index.js
@@ -1,14 +1,9 @@
-var express = require("express");
-const { routes } = require("../app");
+var express = require('express');
+const { routes } = require('../app');
 var router = express.Router();
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
-});
-
-router.use("/ranking", require("./ranking"));
-router.use("/society", require("./society"));
-router.use("/members", require("./members"));
+router.use('/ranking', require('./ranking'));
+router.use('/society', require('./society'));
+router.use('/members', require('./members'));
 
 module.exports = router;
